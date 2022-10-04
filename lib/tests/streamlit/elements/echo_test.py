040d7c1c2378536fbea675ec0f184bb8351ec305
Zachary Blackwood <zachary@streamlit.io>
2022-10-04 12:35:53 -0400

    Add first pass at test

--- lib/tests/streamlit/elements/echo_test.py
+++ lib/tests/streamlit/elements/echo_test.py
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 from parameterized import parameterized
-
 from tests import testutil
+
 import streamlit as st
 
 
@@ -29,6 +29,62 @@ class EchoTest(testutil.DeltaGeneratorTestCase):
     def test_echo(self, _, echo, echo_index, output_index):
         # The empty lines below are part of the test. Do not remove them.
         with echo():
+
+            st.write("Hello")
+
+            "hi"
+
+            def foo(x):
+                y = x + 10
+
+                print(y)
+
+            class MyClass(object):
+                def do_x(self):
+                    pass
+
+                def do_y(self):
+                    pass
+
+        echo_str = """```python
+
+st.write("Hello")
+
+"hi"
+
+def foo(x):
+    y = x + 10
+
+    print(y)
+
+class MyClass(object):
+    def do_x(self):
+        pass
+
+    def do_y(self):
+        pass
+
+
+```"""
+
+        element = self.get_delta_from_queue(echo_index).new_element
+        self.assertEqual(echo_str, element.markdown.body)
+
+        element = self.get_delta_from_queue(output_index).new_element
+        self.assertEqual("Hello", element.markdown.body)
+
+        self.clear_queue()
+
+    @parameterized.expand(
+        [
+            ("code_location default", {}, 0, 1),
+            ("code_location above", {"location": "above"}, 0, 1),
+            ("code_location below", {"location": "below"}, 1, 0),
+        ]
+    )
+    def test_echo_unindent(self, _, echo_kwargs, echo_index, output_index):
+        # The empty lines below are part of the test. Do not remove them.
+        with st.echo(**echo_kwargs):
 
             st.write("Hello")
 
